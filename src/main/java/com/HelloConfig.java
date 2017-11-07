f8275082b2c5b937e6cb8fab3c47ce837facaad8
nitish <nkumarweb1995@gmail.com>
2017-11-07 20:19:47 +0530

    firstgitproject

---
+++ src/main/java/com/HelloConfig.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloConfig {
+	
+	public static void main(String[] args) {
+	SpringApplication.run(HelloConfig.class, args);
+	}
+
+}
---
+++ src/main/java/com/controller/BlogController.java
@@ -0,0 +1,18 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping("/hello")
+public class BlogController {
+	
+	@RequestMapping(value="/helloworld",method=RequestMethod.GET)
+	public String Hello() {
+		System.out.println("in Hello method");
+		return "hello";
+	}
+
+}
